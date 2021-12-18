026095f1fc557019aa325f62fa0cf0f83595b3eb
kamandkrg <kamandkargar@gmail.com>
2021-12-18 14:31:07 +0330

    make classes in model

---
+++ model.py
@@ -0,0 +1,23 @@
+
+
+class Boat:
+    def __init__(self):
+        pass
+
+
+class Mission:
+    def __init__(self):
+        pass
+
+
+class Core:
+    def __init__(self):
+        pass
+
+
+
+
+
+
+
+
