70c17cae4ec96244669aee12609cb805418b3d34
kamandkrg <kamandkargar@gmail.com>
2021-12-18 14:57:41 +0330

    make method for class Boat and class Mission

--- model.py
+++ model.py
@@ -4,9 +4,21 @@ class Boat:
     def __init__(self):
         pass
 
+    def set_boat_pos(self, x, y):
+        pass
+
+    def status(self):
+        pass
+
 
 class Mission:
     def __init__(self):
+        pass
+
+    def end_mission(self):
+        pass
+
+    def add_boat(self):
         pass
 
 
