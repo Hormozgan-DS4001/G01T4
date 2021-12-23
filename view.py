9bed58f943921acfe0ac8e3e56f1f7ec201502f7
kamandkrg <kamandkargar@gmail.com>
2021-12-23 20:12:19 +0330

    make managerpanel

---
+++ view.py
@@ -0,0 +1,10 @@
+from configure import Entry, Tk, TopLevel, Frame, LabelFrame, Button, Scale, Label
+
+
+class ManagerPanel(Tk):
+    def __init__(self):
+        super(ManagerPanel, self).__init__()
+
+
+
+
