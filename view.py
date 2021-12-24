4ef8da02715c6c8bc40f887c3354699de9c6f889
kamandkrg <kamandkargar@gmail.com>
2021-12-24 17:45:48 +0330

    completing init in view

--- view.py
+++ view.py
@@ -27,6 +27,24 @@ class ManagerPanel(Tk):
         Button(frm_btn, text="New Boat", command=self.add_boat).grid(row=0, column=0)
         Button(frm_btn, text="New Mission", command=self.new_mission).grid(row=0, column=1)
 
+        Label(lbl_frm, text="BOATS LIST..↴").grid(row=2, column=0)
+        self.tree_boat = ttk.Treeview(lbl_frm, show="headings", selectmode="browse")
+        self.tree_boat["column"] = ("Name", "Tag", "Crow", "Passenger", "Mission Name")
+        self.tree_boat.heading("Name", text="Name")
+        self.tree_boat.heading("Tag", text="Tag")
+        self.tree_boat.heading("Crow", text="Crow")
+        self.tree_boat.heading("Passenger", text="Passenger")
+        self.tree_boat.heading("Mission Name", text="Mission Name")
+        self.tree_boat.grid(row=3, column=0)
+
+        Label(lbl_frm, text="MISSION LIST..↴").grid(row=4, column=0)
+        self.tree_mis = ttk.Treeview(lbl_frm, show="headings", selectmode="browse")
+        self.tree_mis["column"] = ("Name", "Start Time", "Amount Boat")
+        self.tree_mis.heading("Name", text="Name")
+        self.tree_mis.heading("Start Time", text="Start Time")
+        self.tree_mis.heading("Amount Boat", text="Amount Boat")
+        self.tree_mis.grid(row=5, column=0)
+
     def new_mission(self):
         pass
 
