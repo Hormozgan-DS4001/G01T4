aa6cfd9b3bf910561ffe62638f5d5137e82b4a2f
kamandkrg <kamandkargar@gmail.com>
2021-12-28 17:01:07 +0330

    add show_boat and show_mission to view.py

--- view.py
+++ view.py
@@ -1,3 +1,5 @@
+import datetime
+
 from configure import Entry, Tk, TopLevel, Frame, LabelFrame, Button, Scale, Label
 from tkinter import ttk
 from add_boat_panel import AddBoat
@@ -41,6 +43,7 @@ class ManagerPanel(Tk):
         self.tree_boat.heading("Mission Name", text="Mission Name")
         self.tree_boat.grid(row=3, column=0)
         self.tree_boat.bind("<Double-1>", self.boat_panel)
+        self.list_boat = []
 
         Label(lbl_frm, text="MISSION LIST..↴").grid(row=4, column=0)
         self.tree_mis = ttk.Treeview(lbl_frm, show="headings", selectmode="browse")
@@ -50,6 +53,40 @@ class ManagerPanel(Tk):
         self.tree_mis.heading("Amount Boat", text="Amount Boat")
         self.tree_mis.grid(row=5, column=0)
         self.tree_mis.bind("<Double-1>", self.mission_panel)
+        self.li_mission = []
+
+    def show_boat(self):
+        boats = self.callback_show_boat()
+        self.list_boat = []
+        count = 0
+        for boat in boats:
+            self.list_boat.append(boat)
+            tag = boat.tag[:2] + "-" + boat.tag[2] + "-" + boat.tag[3:]
+            if boat.mission is False:
+                self.tree_boat.insert("", "end", values=(boat.name, tag, boat.crew, boat.passenger, "-"),
+                                      text=str(count))
+            else:
+                self.tree_boat.insert("", 0, values=(boat.name, tag, boat.crew, boat.passenger, boat.mission),
+                                      text=str(count))
+            count += 1
+
+    def show_mission(self):
+        missions = self.callback_show_mission
+        self.li_mission = []
+        count = 0
+        for mission in missions:
+            self.li_mission.append(mission)
+            if mission.status is False:
+                self.tree_mis.insert("", "end", values=(mission.name, "-", mission.boats), text=str(count))
+
+            else:
+                a = datetime.datetime.now()
+                res = a - mission.time
+                self.tree_mis.insert("", 0, values=(mission.name, res.strftime("%y-%m-%d %H:%M:%S"), mission.boats),
+                                     text=str(count))
+            count += 1
+
+
 
     def add_boat(self):
         panel = AddBoat(self.callback_add_boat, self.not_tab)
